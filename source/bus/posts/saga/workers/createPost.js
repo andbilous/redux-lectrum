f98f9571a3d010ad4f52271dadf798348d467f47
andbilous <andbilous@gmail.com>
2019-04-26 23:41:53 +0300

    1.3

--- source/bus/posts/saga/workers/createPost.js
+++ source/bus/posts/saga/workers/createPost.js
@@ -7,13 +7,13 @@ import { uiActions } from '../../../ui/actions';
 export function* createPost ({ payload: comment }) {
     try {
         yield put(uiActions.startFetching());
-        const response = yield apply(api, api.posts.create, [comment]);
+        const response = yield apply(api, api.posts.createPostAsync, [comment]);
         const { data: post, message } = yield apply(response, response.json);
 
         if (response.status !==200) {
             throw new Error(message);
         }
-        yield put(postsActions.createPostAC(post));
+        yield put(postsActions.createPostAsync(post));
         yield put(uiActions.stopFetching());
     } catch (error) {
         yield put(uiActions.emitError(error));
